a07ee33a42114a4c38f6acaef796bcee51d5efe7
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-08-08 18:45:37 +1200

    mvp

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-st.image('./lasha.jpeg', caption='Lasha Lifting in Tokyo 2020. Image source from Infobae')
+st.image('lasha.jpeg', caption='Lasha Lifting in Tokyo 2020. Image source from Infobae')
 
 """
 # How Good Is Lasha?
