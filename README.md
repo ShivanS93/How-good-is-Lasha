7f23832af533d06c0cdfb5213f22b4987edd164f
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-08-08 16:57:26 +1200

    mvp

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# How-good-is-Lasha
+# How good is Lasha?
 
-Looking into parsing Tokyo Olympics data and then seeing how good Lasha is.
+![Image source: https://www.infobae.com/aroundtherings/olympics-games/tokyo-2020/2021/08/04/lasha-talakhadze-smashes-world-and-olympic-records-for-weightlifting-gold/](./lasha.jpeg)
+*Lasha Lifting Image source from [Infobae](https://www.infobae.com/aroundtherings/olympics-games/tokyo-2020/2021/08/04/lasha-talakhadze-smashes-world-and-olympic-records-for-weightlifting-gold/)*
+
+## About
+
+Lasha Talakhadze recent performance in the Tokyo 2020 Olympics was an mind blowing display of strength.
+
+Here lies the insipiration for this project. How good is Lasha? Is there a way to display this graphically?
+
+## Jupyter Notebook
+
+The code is split into two forms, the notebook and a .py file.
+
+The notebook contains code used to wrangle the data and then output this into `results_all.pkl` file.
+
+The notebook contains a lot of explanations as to why I did things a certain way.
+
+Unfortunately, the jupyter notebook uses my conda environment which has a jumble of libraries. The main dependencies on top of conda are:
+- tabula
+- altair 
+
+## main.py file
+
+This takes the `results_all.pkl` file and displays it in an interactive graph hosted using `streamlit`. The `Pipfile` should contain the dependences to run the streamlit application.
+
+```bash
+pipenv run streamlit run main.py
+```
---
+++ main.py
@@ -0,0 +1,52 @@
+#!usr/bin/python3
+# main.py
+
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+st.image('./lasha.jpeg', caption='Lasha Lifting in Tokyo 2020. Image source from Infobae')
+
+"""
+# How Good Is Lasha?
+
+The graph below shows all the z-scores of the lifters in the Tokyo 2020 Olympics.
+
+The z-score is calculated using the difference between the individual's total and the mean total of all lifters in the individual's weight class divided by the standard deviation of the totals.
+
+Calculating z-score:
+"""
+st.latex(r'''
+        z = \frac{x - \mu}{\sigma}
+        '''
+        )
+
+"""
+The colour represents the lifter's ranking within the weight category.
+
+The y-axis is the lifter's placing in their weight category.
+"""
+df = pd.read_pickle('results_all.pkl')
+
+# list for male and female
+cat_lst = list(df['Category'].unique())
+categories = st.container()
+select_all = st.checkbox('Select All', value=True) 
+if select_all:
+    selected_categories = categories.multiselect('Select', cat_lst, cat_lst)
+else:
+    selected_categories = categories.multiselect('Select', cat_lst)
+
+df = df[df['Category'].isin(selected_categories)]
+
+c = alt.Chart(df, title="Z-scores for Tokyo 2020 lifters").mark_circle().encode(
+    y=alt.Y('Rank:Q', title='Placing', scale=alt.Scale(reverse=True)),
+    x=alt.X('Normalised:Q', title='Z-score'),
+    color=alt.Color('Rank:Q', legend=None, scale=alt.Scale(scheme='lightmulti')),
+    tooltip=['Name:N', 'Date of Birth:N', 'Rank:Q', 'Category:N', 'Country:N', 'Final Total:Q'],
+    ).configure_facet(
+        spacing=0
+    ).configure_view(
+        stroke=None
+    )
+st.altair_chart(c, use_container_width=True)
